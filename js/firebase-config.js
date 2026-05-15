3f0b2ebdee4b734888d66dabd95faf6e0970bc0b
Sanjide <sanjidmsnjdp@gmail.com>
2026-05-15 20:57:28 +0530

    Add Firebase configuration and initialization

---
+++ js/firebase-config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore, collection, addDoc, getDocs, query, where, orderBy, onSnapshot, doc, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBV4HUwNtskbvCdhpQNTA4Wvk8oEPyhJlY",
+  authDomain: "financial-b456b.firebaseapp.com",
+  projectId: "financial-b456b",
+  storageBucket: "financial-b456b.firebasestorage.app",
+  messagingSenderId: "137390618798",
+  appId: "1:137390618798:web:48c719ff72cdc4261b953f",
+  measurementId: "G-WWLKBHR8WV"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, db, provider, signInWithPopup, onAuthStateChanged, signOut, collection, addDoc, getDocs, query, where, orderBy, onSnapshot, doc, updateDoc, deleteDoc };
